7ca1254f939baec74d77b02dc9860e0a19543693
Kevin da Silva <kevin.kds80@gmail.com>
2025-12-21 11:34:06 -0300

    add read from remote args build

--- runner/runner.go
+++ runner/runner.go
@@ -1,16 +1,18 @@
 package runner
 
 import (
+	"dumont/config"
 	"dumont/database"
 	"dumont/producers"
 	"fmt"
 )
 
 type RunnerConfig struct {
-	DbConnection *database.Database
-	DateFilter   string
-	BinlogPath   string
-	Producer     *producers.Producer
+	DbConnection         *database.Database
+	DateFilter           string
+	BinlogPath           string
+	Producer             *producers.Producer
+	ReadFromRemoteConfig *config.Config
 }
 
 type CommandExecution struct {
@@ -31,11 +33,25 @@ func (r RunnerConfig) Execute(ch chan CommandExecution) []string {
 
 func (r RunnerConfig) GetArgs(fileName string) []string {
 	args := []string{"--base64-output=decode-rows"}
+	if r.ReadFromRemoteConfig != nil && r.ReadFromRemoteConfig.ReadFromRemote {
+		args = append(args, "--read-from-remote-server")
+		args = append(args, fmt.Sprintf("--host=%s", r.ReadFromRemoteConfig.Host))
+		args = append(args, fmt.Sprintf("--user=%s", r.ReadFromRemoteConfig.User))
+		args = append(args, fmt.Sprintf("--password=%s", r.ReadFromRemoteConfig.Passwd))
+		args = append(args, fmt.Sprintf("--port=%s", r.ReadFromRemoteConfig.Port))
+	}
+
 	if r.DateFilter != "" {
 		args = append(args, fmt.Sprintf("--start-datetime=\"%s\"", r.DateFilter))
 	}
 
 	args = append(args, "-vv")
-	args = append(args, fmt.Sprintf("%s%s", r.BinlogPath, fileName))
+
+	if r.ReadFromRemoteConfig.ReadFromRemote {
+		args = append(args, fileName)
+	} else {
+		args = append(args, fmt.Sprintf("%s%s", r.BinlogPath, fileName))
+	}
+
 	return args
 }
